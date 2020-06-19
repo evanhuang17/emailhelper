f160844ca1a3f7d7e0ebd529cbba5f0b5fc99e34
evanhuang <evanhuang@17.media>
2020-06-19 18:14:20 +0800

    remove handsup and mymai

--- src/Email.js
+++ src/Email.js
@@ -216,10 +216,10 @@ const Email: React.FC = () => {
               <MenuItem value="TW">17LIVE</MenuItem>
               <MenuItem value="JP">17LIVE Japan</MenuItem>
               <MenuItem value="US">17LIVE US</MenuItem>
-              <MenuItem value="HandsUP">HandsUP</MenuItem>
+              {/* <MenuItem value="HandsUP">HandsUP</MenuItem> */}
               <MenuItem value="Wave">Wave</MenuItem>
               <MenuItem value="Unicorn">Unicorn Entertainment</MenuItem>
-              <MenuItem value="17MyMai">17MyMai</MenuItem>
+              {/* <MenuItem value="17MyMai">17MyMai</MenuItem> */}
               <MenuItem value="MeMe">MeMe</MenuItem>
             </Select>
           </FormControl>
