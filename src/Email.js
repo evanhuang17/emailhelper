f26051e06afd09c3d101e338979e0b186ccae485
Evan Huang <evanhuang@M0692-Evan-deMacBook-Pro.local>
2021-08-31 13:59:04 +0800

    rebranding again one seven live

--- src/Email.js
+++ src/Email.js
@@ -31,22 +31,22 @@ const Email: React.FC = () => {
   const [mobile, setMobile] = React.useState("+886-909-090-999");
   const [email, setEmail] = React.useState("xiaomingwang@17.live");
   const [telephone, setTelephone] = React.useState("+886-2-2327-8989 #123");
-  const [organization, setOrganization] = React.useState('HQ');
+  const [organization, setOrganization] = React.useState('TW');
   const [companyName, setCompanyName] = React.useState('17LIVE');
-  const [companyLogo, setCompanyLogo] = React.useState('https://cdn.17app.co/0baab570-cff0-4942-932b-da4cd28dad65.png');
-  const [position, setPosition] = React.useState('Engineer');
-  const [mobileStr, setMobileString] = React.useState(`<b style="color:rgb(7,55,99)">M </b><font color="#666666">${mobile}</font>
-  <b style="color:rgb(7,55,99)"> </b><span style="color:rgb(7,55,99)">|</span>`);
+  const [companyLogo, setCompanyLogo] = React.useState('https://cdn.17app.co/ae338e12-d4d3-4c81-9f76-11c61c23c698.png');
+  const [position, setPosition] = React.useState('VIP');
+  const [mobileStr, setMobileString] = React.useState(`<b style="color:rgb(0,0,0)">M </b><font color="#000000">${mobile}</font>
+  <b style="color:rgb(0,0,0)"> </b><span style="color:rgb(0,0,0)">|</span>`);
   
   const template = `<div style="font-family:Arial,Helvetica,sans-serif">
   <div dir="ltr">
-     <font style="font-size:12px;" color="#073763"><b>${enName} </b>${chName}</font><br>
-     <font color="#3d85c6" style="font-size: 10px;"> ${companyName} | ${position}<br><br></font>
+     <font style="font-size:12px;" color="#000000"><b>${enName} </b>${chName}</font><br>
+     <font color="#000000" style="font-size: 10px;"> ${companyName} | ${position}<br><br></font>
   </div>
-  <div style="font-size: 10px;">${mobileStr}<b style="color:rgb(7,55,99)"> T </b>
-     <font color="#666666">${telephone}</font><font color="#444444">&nbsp;</font><span style="color:rgb(7,55,99)">|</span><b style="color:rgb(7,55,99)"> E </b><font color="#666666">${email}</font><br>
-     <b style="color:rgb(7,55,99)">Follow us : </b><a href="https://www.17live.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.m17.asia/&amp;source=gmail&amp;ust=1589363511983000&amp;usg=AFQjCNHQiHJ6lxRFkz44RyIixkuEyj-27w">
-     <font color="#3d85c6">17LIVE</font></a>
+  <div style="font-size: 10px;">${mobileStr}<b style="color:rgb(0,0,0)"> T </b>
+     <font color="#000000">${telephone}</font><font color="#000000">&nbsp;</font><span style="color:rgb(0,0,0)">|</span><b style="color:rgb(0,0,0)"> E </b><font color="#000000">${email}</font><br>
+     <b style="color:rgb(0,0,0)">Follow us : </b><a href="https://www.17live.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.m17.asia/&amp;source=gmail&amp;ust=1589363511983000&amp;usg=AFQjCNHQiHJ6lxRFkz44RyIixkuEyj-27w">
+     <font color="#000000">17LIVE</font></a>
   </div>
   <div>
     <img src="${companyLogo}" height="42" class="CToWUd">
@@ -83,8 +83,8 @@ const Email: React.FC = () => {
       case 'mobile':
         setMobile(target.value);
         if (target.value)
-          setMobileString(`<b style="color:rgb(7,55,99)">M </b><font color="#666666">${target.value}</font>
-          <b style="color:rgb(7,55,99)"> </b><span style="color:rgb(7,55,99)">|</span>`);
+          setMobileString(`<b style="color:rgb(0,0,0)">M </b><font color="#000">${target.value}</font>
+          <b style="color:rgb(0,0,0)"> </b><span style="color:rgb(0,0,0)">|</span>`);
         else
           setMobileString('');
         break;
@@ -100,40 +100,36 @@ const Email: React.FC = () => {
     setOrganization(target.value);
     switch(target.value) {
       case 'HQ':
-        setCompanyName('17LIVE');
-        setCompanyLogo('https://cdn.17app.co/0baab570-cff0-4942-932b-da4cd28dad65.png');
+        setCompanyName('17LIVE Services Inc.');
+        setCompanyLogo('https://cdn.17app.co/ae338e12-d4d3-4c81-9f76-11c61c23c698.png');
         break;
       case 'TW':
         setCompanyName('17LIVE');
-        setCompanyLogo('https://cdn.17app.co/7fe38e5e-8911-46f5-b32b-106ec3df9010.png');
+        setCompanyLogo('https://cdn.17app.co/ae338e12-d4d3-4c81-9f76-11c61c23c698.png');
         break;
       case 'JP':
         setCompanyName('17LIVE Japan');
-        setCompanyLogo('https://cdn.17app.co/e7cba338-fb2c-457b-95c7-df31f1ba2cdd.png');
+        setCompanyLogo('https://cdn.17app.co/ae338e12-d4d3-4c81-9f76-11c61c23c698.png');
         break;
       case 'US':
         setCompanyName('17LIVE US');
-        setCompanyLogo('https://cdn.17app.co/ba899fd7-ca42-4786-9eba-b3a620ed9a96.png');
+        setCompanyLogo('https://cdn.17app.co/ae338e12-d4d3-4c81-9f76-11c61c23c698.png');
         break;
       case 'Wave':
         setCompanyName('Wave');
-        setCompanyLogo('https://cdn.17app.co/c7764f94-c5f1-4ac9-a777-bd9087f00e17.png');
+        setCompanyLogo('https://cdn.17app.co/1cd5dcec-16b7-4cd0-bba1-a848e94717a4.png');
         break;
       case 'HandsUP':
         setCompanyName('HandsUP');
-        setCompanyLogo('https://cdn.17app.co/e017be35-ab81-4851-81c3-0a3b7aaf9750.png');
-        break;
-      case 'Unicorn':
-        setCompanyName('Unicorn Entertainment');
-        setCompanyLogo('https://cdn.17app.co/366312a6-70ea-4a1b-80ad-f767af57a58d.png');
+        setCompanyLogo('https://cdn.17app.co/b59ac6a6-6470-4a4b-83cd-e1ae3e3cb35f.png');
         break;
       case 'MeMe':
         setCompanyName('MeMe');
-        setCompanyLogo('https://cdn.17app.co/abd59bb7-fe62-410e-9369-5c28a613c8e2.png');
+        setCompanyLogo('https://cdn.17app.co/777284ed-e512-4c40-bd96-6343c6fbd356.png');
         break;
       default:
         setCompanyName('17LIVE');
-        setCompanyLogo('https://cdn.17app.co/0baab570-cff0-4942-932b-da4cd28dad65.png');
+        setCompanyLogo('https://cdn.17app.co/ae338e12-d4d3-4c81-9f76-11c61c23c698.png');
         break;
     }
   };
@@ -217,8 +213,6 @@ const Email: React.FC = () => {
               <MenuItem value="US">17LIVE US</MenuItem>
               <MenuItem value="HandsUP">HandsUP</MenuItem>
               <MenuItem value="Wave">Wave</MenuItem>
-              <MenuItem value="Unicorn">Unicorn Entertainment</MenuItem>
-              {/* <MenuItem value="17MyMai">17MyMai</MenuItem> */}
               <MenuItem value="MeMe">MeMe</MenuItem>
             </Select>
           </FormControl>
